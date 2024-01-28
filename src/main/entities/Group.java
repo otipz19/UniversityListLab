0183ae704eb4b49f28527e2bd87ff98379b2e2db
otipz19 <tasitwork@gmail.com>
2024-01-28 21:11:58 +0200

    Created Group and tests

---
+++ src/main/entities/Group.java
@@ -0,0 +1,26 @@
+package main.entities;
+
+import main.exceptions.validation.InvalidGroupValueValidationException;
+
+public class Group {
+    private final int value;
+
+    public Group(int value){
+        validate(value);
+        this.value = value;
+    }
+
+    private void validate(int value){
+        if(value < 1 || value > 6){
+            throw new InvalidGroupValueValidationException(value);
+        }
+    }
+
+    public boolean equals(Group other){
+        return other.value == value;
+    }
+
+    public String toString(){
+        return Integer.toString(value);
+    }
+}
---
+++ src/tests/entities/GroupTest.java
@@ -0,0 +1,34 @@
+package tests.entities;
+
+import main.entities.Group;
+import main.exceptions.validation.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GroupTest {
+    @Test
+    public void constructorThrowsExceptionOnInvalidValue(){
+        int value = 69;
+
+        assertThrows(InvalidGroupValueValidationException.class, () -> new Group(value));
+    }
+
+    @Test
+    public void constructorDoesNotThrowExceptionOnValidValue(){
+        int rightBorder = 6;
+        int leftBorder = 1;
+
+        assertDoesNotThrow(() -> new Group(rightBorder));
+        assertDoesNotThrow(() -> new Group(leftBorder));
+    }
+
+    @Test
+    public void equalsReturnsTrueByValue(){
+        int value = 1;
+        Group first = new Group(value);
+        Group second = new Group(value);
+
+        assertTrue(first.equals(second));
+    }
+}
