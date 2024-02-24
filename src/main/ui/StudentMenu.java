d0e5f4d2479943a303900edd772ec46e40624503
Svitlana Bublyk <s.bublyk@ukma.edu.ua>
2024-02-24 16:46:03 +0200

    New classes: StudentMenu, TeacherMenu

--- src/main/ui/StudentMenu.java
+++ src/main/ui/StudentMenu.java
@@ -1,4 +1,86 @@
 package main.ui;
 
+import DataInputUtil.main.ConsoleDataReader;
+import main.model.entities.Cathedra;
+import main.model.entities.Student;
+import main.model.valueObjects.Course;
+import main.model.valueObjects.Group;
+import main.model.valueObjects.PersonName;
+
 public class StudentMenu {
+    private Cathedra cathedra;
+    private Student student;
+
+    public StudentMenu(Cathedra cathedra, Student student) {
+        this.cathedra = cathedra;
+        this.student = student;
+    }
+
+    public void start() {
+        while (true) {
+            System.out.println("1. Edit student first name");
+            System.out.println("2. Edit student middle name");
+            System.out.println("3. Edit student last name");
+            System.out.println("4. Edit student course");
+            System.out.println("5. Edit student group");
+            System.out.println("6. Go back to Cathedra");
+            Integer choice = ConsoleDataReader.getInt("Enter your choice: ");
+            switch (choice) {
+                case 1:
+                    editStudentFirstName();
+                    break;
+                case 2:
+                    editStudentMiddleName();
+                    break;
+                case 3:
+                    editStudentLastName();
+                    break;
+                case 4:
+                    editStudentCourse();
+                    break;
+                case 5:
+                    editStudentGroup();
+                    break;
+                case 6:
+                    return;  // Return to Cathedra layer
+                default:
+                    System.out.println("Invalid choice");
+            }
+        }
+    }
+
+    private void editStudentFirstName() {
+        System.out.println("Enter new first name for the student:");
+        PersonName firstName = new PersonName(ConsoleDataReader.getLine());
+        student.setFirstName(firstName);
+        System.out.println("Student first name updated");
+    }
+
+    private void editStudentMiddleName() {
+        System.out.println("Enter new middle name for the student:");
+        PersonName middleName = new PersonName(ConsoleDataReader.getLine());
+        student.setMiddleName(middleName);
+        System.out.println("Student middle name updated");
+    }
+
+    private void editStudentLastName() {
+        System.out.println("Enter new last name for the student:");
+        PersonName lastName = new PersonName(ConsoleDataReader.getLine());
+        student.setLastName(lastName);
+        System.out.println("Student last name updated");
+    }
+
+    private void editStudentCourse() {
+        System.out.println("Enter new course for the student:");
+        Course course = new Course(ConsoleDataReader.getInt());
+        student.setCourse(course);
+        System.out.println("Student course updated");
+    }
+
+    private void editStudentGroup() {
+        System.out.println("Enter new group for the student:");
+        Group group = new Group(ConsoleDataReader.getInt());
+        student.setGroup(group);
+        System.out.println("Student group updated");
+    }
 }
--- src/main/ui/TeacherMenu.java
+++ src/main/ui/TeacherMenu.java
@@ -1,4 +1,62 @@
 package main.ui;
 
+import DataInputUtil.main.ConsoleDataReader;
+import main.model.entities.Cathedra;
+import main.model.entities.Teacher;
+import main.model.valueObjects.PersonName;
+
 public class TeacherMenu {
+    private Cathedra cathedra;
+    private Teacher teacher;
+
+    public TeacherMenu(Cathedra cathedra, Teacher teacher) {
+        this.cathedra = cathedra;
+        this.teacher = teacher;
+    }
+
+    public void start() {
+        while (true) {
+            System.out.println("1. Edit teacher first name");
+            System.out.println("2. Edit teacher middle name");
+            System.out.println("3. Edit teacher last name");
+            System.out.println("4. Go back to Cathedra");
+            Integer choice = ConsoleDataReader.getInt("Enter your choice: ");
+            switch (choice) {
+                case 1:
+                    editTeacherFirstName();
+                    break;
+                case 2:
+                    editTeacherMiddleName();
+                    break;
+                case 3:
+                    editTeacherLastName();
+                    break;
+                case 4:
+                    return;  // Return to Cathedra layer
+                default:
+                    System.out.println("Invalid choice");
+            }
+        }
+    }
+
+    private void editTeacherFirstName() {
+        System.out.println("Enter new first name for the teacher:");
+        PersonName firstName = new PersonName(ConsoleDataReader.getLine());
+        teacher.setFirstName(firstName);
+        System.out.println("Teacher first name updated");
+    }
+
+    private void editTeacherMiddleName() {
+        System.out.println("Enter new middle name for the teacher:");
+        PersonName middleName = new PersonName(ConsoleDataReader.getLine());
+        teacher.setMiddleName(middleName);
+        System.out.println("Teacher middle name updated");
+    }
+
+    private void editTeacherLastName() {
+        System.out.println("Enter new last name for the teacher:");
+        PersonName lastName = new PersonName(ConsoleDataReader.getLine());
+        teacher.setLastName(lastName);
+        System.out.println("Teacher last name updated");
+    }
 }
