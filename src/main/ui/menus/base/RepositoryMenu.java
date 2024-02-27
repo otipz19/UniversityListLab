4cf7715af05b7a34bbbd3897147c590227fa450f
otipz19 <tasitwork@gmail.com>
2024-02-27 22:36:15 +0200

    Added basic pagination

--- src/main/ui/menus/base/RepositoryMenu.java
+++ src/main/ui/menus/base/RepositoryMenu.java
@@ -3,11 +3,10 @@ package main.ui.menus.base;
 import DataInputUtil.main.ConsoleDataReader;
 import DataInputUtil.main.ConsoleUtils;
 import DataInputUtil.main.Option;
-import DataInputUtil.main.OptionsReader;
 import main.model.entities.*;
-import main.model.exceptions.validation.ValidationException;
 import main.model.utils.filtering.*;
 import main.model.utils.list.IMyList;
+import main.model.utils.pagination.Paginator;
 import main.model.valueObjects.*;
 
 public abstract class RepositoryMenu {
@@ -83,8 +82,21 @@ public abstract class RepositoryMenu {
     //TODO: Here have to be pagination implemented.
     protected static <T> void printEntities(String header, IMyList<T> entities) {
         System.out.println(header);
-        for(int i = 0; i < entities.count(); i++){
-            System.out.println((i + 1) + ". " + entities.getAt(i));
+        Paginator<T> paginator = new Paginator<>(5, entities);
+        System.out.println("Pages: " + paginator.getTotalPages());
+        while(paginator.hasNextPage()){
+            IMyList<T> page = paginator.getNextPage();
+            System.out.println("Page#" + paginator.getCurPage() + "\n");
+            for(int i = 0; i < page.count(); i++){
+                int index = ((paginator.getCurPage() - 1) * paginator.getPageSize() + (i + 1));
+                System.out.println(index + ". " + page.getAt(i));
+            }
+            if(paginator.hasNextPage()){
+                String input = ConsoleDataReader.getLine("Input anything to get next page, or 'stop' to stop");
+                if(input.equals("stop")){
+                    break;
+                }
+            }
         }
     }
 }
